2773d89ed872be56f00a0aa0aa9e55fbeccd49d5
Brian Purgert <brianpurgert2@gmail.com>
2017-06-06 16:54:46 -0400

    .

--- gsa_advantage_manufacture_products.rb
+++ gsa_advantage_manufacture_products.rb
@@ -10,8 +10,8 @@ threads     = []
 display_statistics
 
 #----------Normal-----------------Headless-------------#
-Dev_mode ? n_total = 1        : n_total = 5          # Number of Manufactures to search
-Dev_mode ? n_thr = 1          : n_thr = 5             # Number of browsers to run
+Dev_mode ? n_total = 1        : n_total = 15          # Number of Manufactures to search
+Dev_mode ? n_thr = 1          : n_thr = 15             # Number of browsers to run
 gsa_a     = []
 
 
