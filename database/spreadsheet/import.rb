21e7e79d952c4ae803d6122745bedad7901f07b2
Brian Purgert <brianpurgert2@gmail.com>
2017-06-12 19:29:39 -0400

    |

--- database/spreadsheet/import.rb
+++ database/spreadsheet/import.rb
@@ -1,33 +1,49 @@
 require_relative '../../adv/gsa_advantage'
 require 'roo'
-path = "database/spreadsheet/test/Test File 1.xlsx"
 
+def read_client_price_list
 
-xlsx = Roo::Spreadsheet.open('./test/Test File 1.xlsx')
-xlsx = Roo::Excelx.new("./test/Test File 1.xlsx")
+end
 
-puts xlsx.info
+def create_database_table name
+'Mfg Part number'
+'MFG Name'
+'Vendor Name'
+'Lowest Price'
+'URL'
+'Client Price' # (From input table)
+end
 
-xlsx.each_with_pagename do |name, sheet|
+def import_price_list(spreadsheet)
+spreadsheet.each_with_pagename do |name, sheet|
 	p sheet.row(1)
-	@DB.create_table :items do
+	@DB.create_table? :items do
 		primary_key :id
 		String :name
 		Float :price
 	end
 	
-	
-	@DB.create
-		sheet.each(sin: 'SIN',
-		           manufacture_name:       'MFG Name',
-		           manufacture_part:       'MFG Number',
-		           dealer_part_number:     'Dealer Part Number',
-		           product_name:           'Product Name',
-		           product_description:    'Product Description',
-		           gsa_price:              'GSA Price') do |hash|
-		puts hash.inspect
+	data = sheet.parse(clean: true, manufacture_name: 'MFG Name')
+	p data.inspect
+	# header_search:
+	exit
+	sheet.each(
+	# sin: 'SIN',
+	manufacture_name:       'MFG Name',
+	manufacture_part:       'MFG Number',
+	# dealer_part_number:     'Dealer Part Number',
+	# product_name:           'Product Name',
+	# product_description:    'Product Description',
+	gsa_price:              'GSA Price '
+	) do |hash|
+		 p hash.inspect
 	end
 	
 end
+end
 
-
+path = "./test/Test File 1.xlsx"
+xlsx = Roo::Spreadsheet.open(path)
+xlsx = Roo::Excelx.new(path)
+puts xlsx.info
+import_price_list xlsx
