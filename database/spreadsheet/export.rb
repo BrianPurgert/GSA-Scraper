456c6430f3db2fc6fc454b654ef2bb0d88becba3
Brian Purgert <brianpurgert2@gmail.com>
2017-06-20 13:02:21 -0400

    changed word

--- database/spreadsheet/export.rb
+++ database/spreadsheet/export.rb
@@ -18,14 +18,15 @@ def excel(table)
 	    col_header = styles.add_style :bg_color => "FFDFDEDF", :b => true, :alignment => { :horizontal => :center }
 
             wb.add_worksheet(:name => "Overview") do |sheet|
-	                  sheet.add_row ['Mfg Part number',
-	                                 'MFG Name',
+	                  sheet.add_row ['Manufacture Part',
+	                                 'Manufacture Name',
 	                                 'Product',
 	                                 'Lowest Price',
 	                                 'Client Price',
 	                                 'URL',
 	                                 'sources',
-	                                ], :style => [ col_header, col_header, col_header,  col_header,  col_header, col_header]
+	                                 'description',
+	                                ], :style => [ col_header, col_header, col_header,  col_header,  col_header, col_header, col_header]
 	                  
 		result.each do |row|
 			sheet.add_row([row[:mpn],
@@ -34,7 +35,9 @@ def excel(table)
 			              row[:low_price],
 			              row[:gsa_price],
 			              row[:href_name],
-					  row[:sources]])
+					  row[:sources],
+			              row[:desc]
+			              ])
 		end
       p.serialize "#{table.to_s}-PCP.xlsx"
 
