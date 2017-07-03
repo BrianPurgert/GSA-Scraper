1eced2f8f11b2bba47ea73da5f2dcd5cf8747c87
Brian Purgert <brianpurgert2@gmail.com>
2017-07-03 17:10:02 -0400

    added directories related to fbo/sam for reference

---
+++ fbo/fed_biz_opps.rb
@@ -0,0 +1,25 @@
+# https://www.fbo.gov/?&static=interface
+# FBO Electronic Interface: Overview
+
+# FedBizOpps data:  ftp://ftp.fbo.gov/FBOFeed20091012
+
+class FedBizOpps
+	
+	# Page 59 shows an example in php
+	# https://www.fbo.gov/downloads/fbo_web_services_technical_documentation.pdf
+	      Base = 'ftp://ftp.fbo.gov/'
+		Feed = 'FBOFeed'
+		Date = ""   # Format YYYYMMDD
+	
+	def initialize(date = Time.now.strftime("%Y%m%d"))
+		# yday = Time.now - (3600 * 24)
+		# puts Time.now - 1.day
+		#
+			puts "#{Base}#{Feed}#{}"
+		end
+		
+
+end
+
+FedBizOpps.new
+# fbo_files
---
+++ sam/system_award_management.rb
@@ -0,0 +1,30 @@
+# SAM is basically the GSA's directory for business entity’s
+#
+# System for Award Management API
+# API             https://github.com/GSA/sam_api
+# Docs            http://gsa.github.io/sam_api/sam/
+# GSA Database    http://gsa.github.io/sam_api/sam/SAM_Functional_Data_Dictionary_v7_Public.pdf
+# SAM API is a GET API which has one operation. (retrieve an entity’s public information)
+# Its endpoint is:
+# https://api.data.gov/sam/v1/registrations/
+#
+#
+# System for Award Management Data Extracts
+# https://gw.sam.gov/SAMWS/1.0/Entity?wsdl
+# https://gw.sam.gov/SAMWS/1.0/Entity
+# https://www.sam.gov/sam/transcript/SAM_Entity_Management_Public_Extract_Layout_v1.1.pdf
+
+class SystemAwardManagement
+	
+	# Probably should just scrape the json urls and map them to tables
+	
+	'http://gsa.github.io/sam_api/static/businessTypes.json'
+	'http://gsa.github.io/sam_api/static/purpose.json'
+	'http://gsa.github.io/sam_api/static/country.json'
+	'http://gsa.github.io/sam_api/static/county.json'
+	'http://gsa.github.io/sam_api/static/far.json'
+	'http://gsa.github.io/sam_api/static/dfar.json'
+	'http://gsa.github.io/sam_api/static/discipline.json'
+	'http://gsa.github.io/sam_api/static/experience.json'
+	'http://gsa.github.io/sam_api/static/revenue.json'
+end
